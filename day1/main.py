768516a9423954337c1aedf7c105a81e0f5b79ed
Jonny Pissman <beatmy@wife.lol>
2022-12-01 07:40:09 +0100

    Day 1

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,4 @@
-def find_most_valuable_elf(filename="input.txt"):
+def find_elf_values(filename="input.txt"):
 
     index = 1
     elf_values = {}
@@ -7,17 +7,27 @@ def find_most_valuable_elf(filename="input.txt"):
     with open(filename, "r") as file:
         for line in file.readlines():
             if line.strip().isalnum():
-                print(line.strip())
                 temp_value += int(line.strip())
             else:
                 elf_values[index] = temp_value
                 temp_value = 0
                 index += 1
 
-    return max(elf_values.values())
+    return elf_values
 
 
 if __name__ == "__main__":
-    print(find_most_valuable_elf())
+    elf_values = find_elf_values()
+    print(max(elf_values.values()))
+
+    # part 2 here
+    # bad solution
+    temp = []
+    for i in elf_values.values():
+        temp.append(i)
+    temp.sort()
+    part2 = temp[-1] + temp[-2] + temp[-3]
+    print(part2)
+
 else:
     raise BaseException
