7ddf8de9456148045b212316af65aa69e8b45016
Jonny Pissman <beatmy@wife.lol>
2022-12-03 12:55:10 +0100

    day 3

--- day3/main.py
+++ day3/main.py
@@ -10,18 +10,37 @@ def p1(data):
             if char in part_2:
                 if char.islower():
                     priority_sum += ord(char) - 96
-                    print(ord(char) - 96)
-                    print(char)
                     break
                 else:
                     priority_sum += ord(char) - 38
-                    print(ord(char) - 38)
-                    print(char)
                     break
 
     return priority_sum
 
-def p2(data)
+
+def p2(data):
+    priority_sum = 0
+    only_data = []
+    for i in data.values():
+        only_data.append(i)
+    for i in range(0, len(only_data), 3):
+        group = only_data[i:i+3]
+        bag1 = group[0]
+        bag2 = group[1]
+        bag3 = group[2]
+
+        for char in bag1:
+            if char in bag2 and char in bag3:
+                if char.islower():
+                    priority_sum += ord(char) - 96
+                    break
+                else:
+                    priority_sum += ord(char) - 38
+                    break
+
+    return priority_sum
+
+
 if __name__ == "__main__":
     bag_number = 1
     data = {}
@@ -29,6 +48,7 @@ if __name__ == "__main__":
         for line in file.readlines():
             data[bag_number] = line.strip()
             bag_number += 1
-    print(p1(data))
+    print(p2(data))
+
 else:
     raise BaseException
